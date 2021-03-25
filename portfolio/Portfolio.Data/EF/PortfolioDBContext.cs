1514978fb165e47f6b8736e1f2da0989664a25a8
vinhlp32aptech <vinh.lp32@aptechlearning.edu.vn>
2021-03-25 21:08:47 +0700

    add db class vinh

---
+++ portfolio/Portfolio.Data/EF/PortfolioDBContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Portfolio.Data.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Data.EF
+{
+    class PortfolioDBContext : DbContext
+    {
+        public PortfolioDBContext( DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<SocialUser> SocialUsers { get; set; }
+        public DbSet<View> views { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+        public DbSet<Follow> Follows { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+
+    }
+}
---
+++ portfolio/Portfolio.Data/Entites/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Data.Entites
+{
+    public class User
+    {
+		public string id_user { get; set; }
+		public string first_name { get; set; }
+		public string last_name { get; set; }
+		public string avatar { get; set; }
+		public DateTime dob { get; set; }
+		public string phone_number { get; set; }
+		public string addr { get; set; }
+		public string website { get; set; }
+		public string job { get; set; }
+		public int project { get; set; }
+		public string id_acc { get; set; }
+
+
+	}
+}
