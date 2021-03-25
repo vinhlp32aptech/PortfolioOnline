3f1b87b9b3bc7427a6e2d11821c52ca5dfe2f229
vinhlp32aptech <vinh.lp32@aptechlearning.edu.vn>
2021-03-25 19:26:51 +0700

    aaa

---
+++ portfolio/PortfolioOnline/Areas/Admin/Controllers/PageController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortfolioOnline.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("page")]
+    [Route("admin/page")]
+    public class PageController : Controller
+    {
+
+        [Route("slider")]
+        public IActionResult Index()
+        {
+            return View("slider");
+        }
+
+        [Route("content_page")]
+        public IActionResult content_page()
+        {
+            return View("ContentPage");
+        }
+        [Route("session_page")]
+        public IActionResult session_page()
+        {
+            return View("SessionPage");
+        }
+
+    }
+}
