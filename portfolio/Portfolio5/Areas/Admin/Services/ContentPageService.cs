c0d2bdbdaf9877ed75e3701a54f6c751a284ddb8
Vinh Le <lephatvinh8464@hotmail.com>
2021-04-08 15:02:59 +0700

    vinh add model service

---
+++ portfolio/Portfolio5/Areas/Admin/Services/ContentPageService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio5.Areas.Admin.Services
+{
+    public interface ContentPageService
+    {
+    }
+}
